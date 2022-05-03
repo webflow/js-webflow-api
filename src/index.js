d63a0610d1a948959d442ca592713f3d1603add6
mfnmatt <104689203+mfnmatt@users.noreply.github.com>
2022-05-02 21:44:16 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-module.exports = require("./Webflow").default;
-module.exports.WebflowError = require("./WebflowError").default;
+// module.exports = require("./Webflow").default;
+// module.exports.WebflowError = require("./WebflowError").default;
+async function WebflowFunc() {
+const { Webflow } = await import('./Webflow.mjs')
+}
+WebflowFunc()
+
+async function WebflowErrorFunc() {
+const { WebflowError } = await import('./WebflowError.mjs')
+}
+WebflowErrorFunc()
