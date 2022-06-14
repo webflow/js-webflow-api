d7f79274fcb6affe7428c11efa475d06bc7cd889
John Agan <john.agan@webflow.com>
2022-06-14 10:22:44 -0700

    added prettier support for eslint

--- src/WebflowError.js
+++ src/WebflowError.js
@@ -1,6 +1,5 @@
-import ExtendableError from 'es6-error';
+import ExtendableError from "es6-error";
 
 export default class WebflowError extends ExtendableError {}
 
-export const buildRequiredArgError = name =>
-  new WebflowError(`Argument '${name}' is required but was not present`);
+export const buildRequiredArgError = (name) => new WebflowError(`Argument '${name}' is required but was not present`);
