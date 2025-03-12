e8bedb29f0ae594c4807aff78dd8e0bb518826f3
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-12 16:38:16 +0000

    SDK regeneration

--- src/api/resources/sites/resources/index.ts
+++ src/api/resources/sites/resources/index.ts
@@ -1,5 +1,6 @@
 export * as redirects from "./redirects";
 export * as plans from "./plans";
+export * as robotsTxt from "./robotsTxt";
 export * as activityLogs from "./activityLogs";
 export * as scripts from "./scripts";
 export * from "./activityLogs/client/requests";
